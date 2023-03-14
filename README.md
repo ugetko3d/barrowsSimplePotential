ce3afa116d7bcfea98871e2d6946c32c093da2cc
Vetle Deilkås <vetled64@gmail.com>
2023-03-14 22:01:10 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Example
-An example greeter plugin
+# BarrowsSimplePotential
+A barrows potential plugin that will display which monsters to kill to get maximum runes.
+It's very simple, it will basically tell you to kill 1x skeleton, 1x bloodworm, and 2x crypt spiders in the tunnels beneath the crypts.
+The plugin assumes that you also kill all 6 barrows brothers.
+This will give a total of 880 potential points, or 88.0%, which will have the best chance of giving maximum amount of runes.
--- src/main/java/com/barrowssimplepotential/BarrowsSimplePotentialPlugin.java
+++ src/main/java/com/barrowssimplepotential/BarrowsSimplePotentialPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.barrowssimplepotential;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "BarrowsSimplePotential"
 )
-public class ExamplePlugin extends Plugin
+public class BarrowsSimplePotentialPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private BarrowsSimplePotentialConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	BarrowsSimplePotentialConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(BarrowsSimplePotentialConfig.class);
 	}
 }
